991dbebf8e2d4a42ca99f68166b2fa853e88e6e2
Victor Cifuentes <vdcifuentesg@uqvirtual.edu.co>
2023-04-22 10:09:20 -0500

    Edición del codigo para que permita generar un archivo JSON con todos los datos de los diversos excel

--- index.js
+++ index.js
@@ -9,14 +9,13 @@ fs.readdir('archivos', (err, files) => {
     return;
   }
 
+  const result = {}; // Objeto global para almacenar los datos de todas las hojas
+
   files.forEach(file => {
     const filePath = path.join('archivos', file);
     const workbook = xlsx.readFile(filePath);
 
-    const jsonData = {};
-
     workbook.SheetNames.forEach(sheetName => {
-      // Ignorar hojas con el nombre 'general' (en mayúsculas o minúsculas)
       if (sheetName.toLowerCase() === 'general') {
         return;
       }
@@ -24,31 +23,41 @@ fs.readdir('archivos', (err, files) => {
       const worksheet = workbook.Sheets[sheetName];
       const data = xlsx.utils.sheet_to_json(worksheet);
 
-      // Buscar las columnas con formato de año 'YYYY'
-      for (let i = 0; i < data.length; i++) {
-        const row = data[i];
-        const keys = Object.keys(row);
-
-        for (let j = 0; j < keys.length; j++) {
-          const key = keys[j];
-          const value = row[key];
-        }
+      const fileName = path.parse(file).name; // Obtener el nombre del archivo sin la extensión
+      if (!result.hasOwnProperty(sheetName)) {
+        result[sheetName] = {};
       }
 
-      // Guardar la hoja en un objeto JSON con el mismo nombre de la hoja
-      jsonData[sheetName] = data;
-    });
+      result[sheetName][fileName] = data.map(row => {
+        const nameColumns = Object.keys(row).filter(key => {
+          return typeof row[key] === 'string' &&
+            (key.toLowerCase().includes('nombre') || key.toLowerCase().includes('apellido'));
+        });
 
-    // Guardar el objeto JSON en un archivo con el nombre original del archivo Excel
-    const jsonFileName = file.replace('.xlsx', '.json');
-    const jsonFilePath = path.join('archivos', jsonFileName);
-    fs.writeFile(jsonFilePath, JSON.stringify(jsonData, null, 2) + os.EOL, (err) => {
-      if (err) {
-        console.error(`Error al guardar el archivo \${jsonFileName}:`);
-        console.error(err);
-        return;
-      }
-      console.log(`Archivo \${jsonFileName} guardado correctamente`);
+        const nameValues = nameColumns.map(key => row[key]);
+        const jugadorName = nameValues.join(' ');
+
+        const newRow = { Jugador: { name: jugadorName } };
+        Object.keys(row).forEach(key => {
+          if (!nameColumns.includes(key)) {
+            newRow.Jugador[key] = row[key];
+          }
+        });
+
+        return newRow;
+      });
     });
+  });
+
+  // Guardar el objeto 'result' en un archivo JSON con el nombre "historico.json"
+  const jsonFileName = 'historico.json';
+  const jsonFilePath = path.join('archivos', jsonFileName);
+  fs.writeFile(jsonFilePath, JSON.stringify(result, null, 2) + os.EOL, (err) => {
+    if (err) {
+      console.error(`Error al guardar el archivo \${jsonFileName}:`);
+      console.error(err);
+      return;
+    }
+    console.log(`Archivo \${jsonFileName} guardado correctamente`);
   });
 });

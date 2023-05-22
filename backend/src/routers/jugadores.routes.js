efb8237abc6c1b43de249929f2f7f9929837b8cd
juanptoarr <juanp.torresa@uqvirtual.edu.co>
2023-05-22 18:21:08 -0500

    Consultas a la DB listas

--- backend/src/routers/jugadores.routes.js
+++ backend/src/routers/jugadores.routes.js
@@ -4,6 +4,9 @@ const router = Router();
 const jugadoresController = require('../controllers/competicion.controller')
 
 router.get('/',jugadoresController.getCompeticiones)
+router.get('/:competicion/:tipodatos',jugadoresController.getCompeticionTipoDatos)
+router.get('/:competicion',jugadoresController.getCompeticionTipoDatos)
+router.get('/',jugadoresController.getCompeticiones)
 router.post('/',jugadoresController.createJugador)
 router.put('/:id',jugadoresController.updateJugador)
 router.delete('/:id',jugadoresController.deleteJugador)
